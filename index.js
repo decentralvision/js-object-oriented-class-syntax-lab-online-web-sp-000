499c87090acb3f88b86a2f62dcdc5326ec448c4e
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 19:30:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class BoardMember {
   approve() { return `You can do that!`}
   doCharity() { return `I like to help people.`}
   releasePressStatement() { return `You will see great things from Scuber.`}
+  sayHi() {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
 // Add the following methods to the prototype using the class syntax:
 // + `veto` — returns `No, I must disagree`
