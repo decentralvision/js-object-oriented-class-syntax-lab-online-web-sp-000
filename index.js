4bea6b3fb03d423a9ba4e93707021894688d22b6
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 19:33:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ class BoardMember {
   sayHi() {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
 
-class Ceo {
-  constructor()
+class Ceo extends BoardMember {
 }
 // Add the following methods to the prototype using the class syntax:
 // + `veto` — returns `No, I must disagree`
