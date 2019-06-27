18a15d8c1f8dff0d6d4f98a03187c7ae3f07e845
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 19:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ class BoardMember {
   sayHi() {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
 
-// class
+class Ceo {
+  constructor()
+}
 // Add the following methods to the prototype using the class syntax:
 // + `veto` — returns `No, I must disagree`
 //
